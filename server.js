68756bc8baea2e24c312f642dd64e260fda228df
Alex Pauwels <alexpauwels@MacBook-Pro-van-Alex.local>
2020-05-07 23:04:11 +0200

    Initial release

---
+++ server.js
@@ -0,0 +1,49 @@
+// Setup empty JS object to act as endpoint for all routes
+let projectData = [];
+
+// Require Express to run server and routes
+const express = require('express');
+
+// Start up an instance of app
+const app = express();
+
+// Dependencies
+const bodyParser = require('body-parser')
+
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
+
+// Initialize the main project folder
+app.use(express.static('website'));
+
+// Setup Server
+const port = 3000;
+const server = app.listen(port, listening);
+function listening(){
+    console.log(`Running on localhost: ${port}`);
+}
+
+// Routes
+app.get('/all', sendData);
+
+function sendData (request, response) {
+    response.send(projectData);
+};
+
+app.post('/addJournalData', addJournalData);
+
+function addJournalData (request, response){
+    newEntry = {
+        date: request.body.date,
+        temp: request.body.temp,
+        content: request.body.content
+    }
+    projectData = []
+    projectData.push(newEntry);
+};
---
+++ website/javascript/app.js
@@ -0,0 +1,85 @@
+// Personal API Key for OpenWeatherMap API
+const apiUrl = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+const apiKey = '&appid=57d8704d83b47e11e5115af0dc5cff52';
+
+/* Global Variables */
+let tempUnit = '&units=imperial';
+let tempUnitText = ' °F';
+
+// Event listener to add function to existing HTML DOM element
+document.getElementById('generate').addEventListener('click', buttonClicked);
+
+/* Function called by event listener */
+function buttonClicked() {
+    let zipCode = document.getElementById('zip').value;
+    let feelings = document.getElementById('feelings').value;
+    let countryCode = document.getElementById('country').value;
+    if (countryCode == ',be') {
+        tempUnit = '&units=metric';
+        tempUnitText = ' °C';
+    }
+    let urlConstruct = apiUrl + zipCode + countryCode + apiKey + tempUnit;
+    getWeather(urlConstruct)
+    .then(function(data){
+        let date = new Date(data.dt * 1000)
+        let date_str = date.getDate() + '/' + (date.getMonth()+1) + '/' + date.getFullYear();
+        postData('/addJournalData', {date: date_str, temp: data.main.temp + tempUnitText, content: feelings})
+        updateView('/all');
+    })
+}
+
+/* Function to GET Web API Data*/
+const getWeather = async ( url = '') =>{ 
+
+    const res = await fetch(url);
+
+    try {
+        // Transform into JSON
+        let data = await res.json()
+        return data;
+    }
+    catch(error) {
+        console.log("error", error);
+        // appropriately handle the error
+    }
+};
+
+/* Function to POST data */
+// Async POST
+const postData = async ( url = '', data = {})=>{
+
+    const response = await fetch(url, {
+        method: 'POST', 
+        credentials: 'same-origin', 
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data), // body data type must match "Content-Type" header        
+    });
+
+    try {
+        let newData = await response.json();
+    }catch(error) {
+        console.log("error", error);
+    }
+};
+
+/* Function to GET Project Data */
+let updateView = async ( url = '') => {
+
+    let request = await fetch(url);
+
+    try {
+        let dataJournal = await request.json();
+        document.getElementById('date').innerHTML = dataJournal[0].date;
+        document.getElementById('temp').innerHTML = dataJournal[0].temp;
+        document.getElementById('content').innerHTML = dataJournal[0].content;
+        document.querySelector('.entry').style.display = "block";
+    } catch(error) {
+        console.log('error', error);
+    };
+};
+
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
